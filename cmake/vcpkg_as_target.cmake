38d60bc7bf46a77fe19fbf0c2451c6c88891b646
luncliff <luncliff@gmail.com>
2019-11-05 12:06:55 +0900

    cmake: move add_custom_command

--- cmake/vcpkg_as_target.cmake
+++ cmake/vcpkg_as_target.cmake
@@ -1,16 +1,18 @@
 cmake_minimum_required(VERSION 3.13)
 
-add_library(vcpkg_meta INTERFACE)
 if(${CMAKE_TOOLCHAIN_FILE} MATCHES vcpkg.cmake)
     message(STATUS "Vcpkg")
     message(STATUS "  Path      \t: ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}")
     message(STATUS)
-    target_include_directories(vcpkg_meta
-    INTERFACE
-        ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include
-    )
-    target_link_directories(vcpkg_meta
-    INTERFACE
-        ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib
-    )
+
+    set(VCPKG_INCLUDE_DIR ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include)
+    set(VCPKG_LIB_DIR     ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib)
+    # target_include_directories(vcpkg_meta
+    # PRIVATE
+    #     ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include
+    # )
+    # target_link_directories(vcpkg_meta
+    # PRIVATE
+    #     ${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib
+    # )
 endif()
